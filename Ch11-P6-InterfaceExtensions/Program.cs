c2c67148fedfd826854a95504718e3cab0d01903
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-10-23 15:19:25 +0500

    Project Created.

---
+++ Ch11-P6-InterfaceExtensions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11_P6_InterfaceExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
