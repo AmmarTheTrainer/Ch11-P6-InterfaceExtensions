7363b12a2d5e16b154c2d18e8dc8c6385f26580a
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-10-23 16:07:58 +0500

    code done

---
+++ Ch11-P6-InterfaceExtensions/IMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11_P6_InterfaceExtensions
+{
+    public interface IMath
+    {
+
+    }
+
+    public class Math : IMath
+    {
+        
+    }
+    public class Math2 : IMath
+    {
+
+    }
+
+    static class MathExtensions
+    {
+        public static void MathExtensionMethods(this IMath im)
+        {
+
+        }
+    }
+}
--- Ch11-P6-InterfaceExtensions/Program.cs
+++ Ch11-P6-InterfaceExtensions/Program.cs
@@ -10,7 +10,26 @@ namespace Ch11_P6_InterfaceExtensions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Extending Interface Compatible Types *****\n");
+            
+            // System.Array implements IEnumerable!
+            string[] data = { "Wow", "this", "is", "sort", "of", "annoying", "but", "in", "a", "weird", "way", "fun!"};
+            data.PrintDataAndBeep();
 
+            Console.WriteLine();
+            
+            // List<T> implements IEnumerable!
+            List<int> myInts = new List<int>() { 10, 15, 20 };
+            myInts.PrintDataAndBeep();
+
+            //// internet surfing
+
+            ////Math math = new Math();
+            ////math.
+            //Math2 math = new Math2();
+            //math.
+
+            Console.ReadLine();
         }
     }
 }
